8a0c275655b7b8ff614361bef7dc312a1d467d0c
Krunal2101 <krunalpatel6821@gmail.com>
2025-04-03 14:49:57 +0000

    learn variables

---
+++ 01_basics/01_varable.js
@@ -0,0 +1,15 @@
+const AccountId = 2101
+let AccountEmail = "krunal@123"
+var AccountPassword = "124"
+AccountCity = "Surat"
+
+// AccountId = 21 
+
+console.log(AccountId);
+
+AccountEmail = "ks@21"
+AccountPassword = "2121"
+AccountCity = "Mumbai"
+let AccountState; 
+
+console.table([AccountId, AccountEmail, AccountPassword, AccountCity, AccountState])
