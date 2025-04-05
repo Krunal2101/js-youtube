10fe5f421265550b7e4413cc3dd447bc8cd58332
Krunal2101 <krunalpatel6821@gmail.com>
2025-04-05 06:40:01 +0000

    Conversion of datatype

---
+++ 01_basics/02_conversion&opration.js
@@ -0,0 +1,26 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+// let ValueInNumber = number(score)
+//console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+// ture => 1 & false => 0
+//null => 0
+
+let loggedIn = 0
+//let IsloggedIn = Boolean(loggedIn)
+
+//console.log(IsloggedIn);
+
+// ""=> false
+// "Krunal" => true
+
+let Number = 21
+let StringNumber = String(Number)
+
+console.log(StringNumber);
