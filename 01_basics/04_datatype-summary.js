e78b89869354e925b576779c6e57091d5196e72d
Krunal2101 <krunalpatel6821@gmail.com>
2025-04-08 15:20:51 +0000

    datatype summary

---
+++ 01_basics/04_datatype-summary.js
@@ -0,0 +1,30 @@
+// primitive 
+// 7 type : string, number, boolearn, null, nudefind, symbol, BigInt
+
+const Score = 100
+const ScoreValue = 21.21
+const LoggedIn = true
+const Temp = null
+let userEmail;
+
+const BidNum = 1518645548n
+
+const Id = Symbol("21")
+const newId = Symbol('21')
+
+console.log(Id === newId);
+
+// refernce (Non Primitive) 
+// 3 type: Array, Object, Function
+
+const heros = ["luffy", "Zoro", "Sanji"]
+
+let Object = {
+    name: "Krunal",
+    age: 22,
+}
+
+const MyFunction = function() {
+    console.log("Hello World");
+    
+}
