c307572104adae1a1f1e4351df60d1a3de332475
Krunal2101 <krunalpatel6821@gmail.com>
2025-04-08 07:31:04 +0000

    conversions

---
+++ 01_basics/03_comparision.js
@@ -0,0 +1,15 @@
+// basic conversion
+// console.log(1 < 2);
+// console.log(1 <= 2);
+// console.log(1 > 2);
+// console.log(1 == 2);
+// console.log(1 != 2);
+
+// avoid this type of conversion
+console.log(null < 0);
+console.log(null > 0);
+console.log(null <= 0);
+
+
+// === stric check
+
