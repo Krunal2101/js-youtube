e8446e8562b593726618d3151cb57388dae9c701
Krunal2101 <krunalpatel6821@gmail.com>
2025-04-10 06:37:07 +0000

    Math and Num

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,31 @@
+const score = 400
+// console.log(score);
+
+const blance = new Number(100)
+// console.log(blance);
+
+// console.log(blance.toFixed(1));
+
+const otherscore = 124.892121
+
+// console.log(otherscore.toPrecision(3));
+
+// ************************** Maths ****************************** //
+
+
+// console.log(Math);
+// console.log(Math.abs(-21));
+// console.log(Math.round(21.6));
+// console.log(Math.ceil(2.3));
+// console.log(Math.floor(2.8));
+// console.log(Math.min(2, 5, 8, 0));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random()* 10) + 1);
+
+const max = 20
+const min = 10
+
+console.log(Math.floor(Math.random()* (max - min + 1)) + min);
+ 
