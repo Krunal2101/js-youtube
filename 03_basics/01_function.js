6c3c5d24db52f74d9c3a4a7fc6c74ce0e13c08a6
Krunal2101 <krunalpatel6821@gmail.com>
2025-07-30 08:23:17 +0000

    funct

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -24,4 +24,32 @@ function loginusermessage (username){
 }
 
 // console.log(loginusermessage("krunal"))
-console.log(loginusermessage())
+// console.log(loginusermessage())
+
+
+function calculatecartprice(...num1){
+    return num1;
+}
+
+// console.log(calculatecartprice(100,300,800));
+
+const user = {
+    name:"Krunal",
+    price: 199
+}
+
+function handelobject(anyobject){
+    console.log(`username is ${anyobject.name} and price is ${anyobject.price}`)
+}
+
+// handelobject(user);
+
+//using arry for this method
+
+const newarry = [200,400,500]
+
+function returnsecondvalue(anyarry){
+    return anyarry[2]
+}
+
+console.log(returnsecondvalue(newarry))
