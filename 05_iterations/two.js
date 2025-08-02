c6605215e4ff310060fdef6e9c636a0df2e422a7
Krunal2101 <krunalpatel6821@gmail.com>
2025-08-02 08:55:34 +0000

    while & do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+
+let index = 0
+while (index <= 10) {
+    // console.log(`index num is ${index}` );
+    index = index + 2
+}
+
+let myarray = ['krunal', 'himay', 'milan']
+
+let arr = 0
+while (arr < myarray.length) {
+    //console.log(`num of array ${myarray[arr]}`);
+    arr = arr +1
+}
+
+// do while
+
+let score = 1
+do {
+    console.log(`num of score ${score}`);
+    score = score+1
+} while (score<= 10);
